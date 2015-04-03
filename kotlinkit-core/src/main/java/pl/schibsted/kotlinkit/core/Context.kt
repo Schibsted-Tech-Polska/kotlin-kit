38353036796424179a2301961a9cfdbf16dfaedc
Jacek Kwiecień <jacek.kwiecien@schibsted.pl>
2015-04-03 12:27:07 +0200

    splited modules

---
+++ kotlinkit-core/src/main/java/pl/schibsted/kotlinkit/core/Context.kt
@@ -0,0 +1,33 @@
+package pl.schibsted.core.extensions
+
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+/**
+ * Created by Jacek Kwiecień on 25.03.15.
+ */
+
+fun Context.hideKeyboard(caller: View) {
+    caller.postDelayed(object : Runnable {
+        override fun run() {
+            try {
+                val imm = caller.getContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(caller.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
+            } catch (e: Exception) {
+                Log.w(javaClass.getSimpleName(), "Could not hide keyboard. " + e.getMessage())
+            }
+
+        }
+    }, 300)
+}
+
+fun Context.showKeyboard(caller: View) {
+    caller.postDelayed(object : Runnable {
+        override fun run() {
+            val imm = caller.getContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(caller, InputMethodManager.SHOW_IMPLICIT)
+        }
+    }, 100)
+}
---
+++ kotlinkit-core/src/main/java/pl/schibsted/kotlinkit/core/Fragment.kt
@@ -0,0 +1,32 @@
+package pl.schibsted.core.extensions
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+
+/**
+ * Created by Jacek Kwiecień on 01.04.15.
+ */
+
+fun <T : Activity> Fragment.startActivity(activityToStart: Class<T>) {
+    val intent = Intent(getActivity(), activityToStart)
+    startActivity(intent)
+}
+
+fun <T : Activity> Fragment.startActivityForResult(activityToStart: Class<T>, requestCode: Int) {
+    val intent = Intent(getActivity(), activityToStart)
+    startActivityForResult(intent, requestCode)
+}
+
+fun <T : Activity> Fragment.startActivity(activityToStart: Class<T>, extras: Bundle) {
+    val intent = Intent(getActivity(), activityToStart)
+    intent.putExtras(extras)
+    startActivity(intent)
+}
+
+fun <T : Activity> Fragment.startActivityForResult(activityToStart: Class<T>, extras: Bundle, requestCode: Int) {
+    val intent = Intent(getActivity(), activityToStart)
+    intent.putExtras(extras)
+    startActivityForResult(intent, requestCode)
+}
