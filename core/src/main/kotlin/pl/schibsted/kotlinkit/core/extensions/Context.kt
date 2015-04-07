1a114fc457ad9a50ee57f7c55c9c878653e576c6
Damian Petla <damian.petla@schibsted.pl>
2015-04-07 09:43:01 +0200

    removed anonymous runnable

--- core/src/main/kotlin/pl/schibsted/kotlinkit/core/extensions/Context.kt
+++ core/src/main/kotlin/pl/schibsted/kotlinkit/core/extensions/Context.kt
@@ -10,24 +10,19 @@ import android.view.inputmethod.InputMethodManager
  */
 
 fun Context.hideKeyboard(caller: View) {
-    caller.postDelayed(object : Runnable {
-        override fun run() {
-            try {
-                val imm = caller.getContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(caller.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
-            } catch (e: Exception) {
-                Log.w(javaClass.getSimpleName(), "Could not hide keyboard. " + e.getMessage())
-            }
-
+    caller.postDelayed({
+        try {
+            val imm = caller.getContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(caller.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
+        } catch (e: Exception) {
+            Log.w(javaClass.getSimpleName(), "Could not hide keyboard. " + e.getMessage())
         }
     }, 300)
 }
 
 fun Context.showKeyboard(caller: View) {
-    caller.postDelayed(object : Runnable {
-        override fun run() {
-            val imm = caller.getContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(caller, InputMethodManager.SHOW_IMPLICIT)
-        }
+    caller.postDelayed({
+        val imm = caller.getContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(caller, InputMethodManager.SHOW_IMPLICIT)
     }, 100)
 }
